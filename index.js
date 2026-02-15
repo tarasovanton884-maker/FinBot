18a7f0119ce970447ac686a161be87f1ac165333
tarasovanton884-maker <tarasovanton884@gmail.com>
2026-02-15 16:36:41 +0100

    Use environment variable for server port

--- index.js
+++ index.js
@@ -23,4 +23,5 @@ app.post('/subscribe', (req, res) => {
     res.send({ url: 'https://buy.stripe.com/test_subscription_link' });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
